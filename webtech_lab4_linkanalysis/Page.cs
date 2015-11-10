0d942f3803cf59142bb69de4f3ffb5e7b1405df6
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2015-11-10 19:42:34 +0000

    Converts the link file into an object

---
+++ webtech_lab4_linkanalysis/Page.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webtech_lab4_linkanalysis
+{
+    public class Page
+    {
+        public string pageUrl { get; set; } //the HTML page
+        
+        public List<String> links { get; set; } //links contained within the HTML page
+
+        public Page(string pageUrl) { this.pageUrl = pageUrl; links = new List<string>(); }
+    }
+}
--- webtech_lab4_linkanalysis/Program.cs
+++ webtech_lab4_linkanalysis/Program.cs
@@ -3,13 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
 namespace webtech_lab4_linkanalysis
 {
     class Program
     {
+        static List<Page> visitedPages = new List<Page>(); //the pages that have been visited
+        const string LINKFILE = @"http://www.dcs.bbk.ac.uk/~martin/sewn/ls4/sewn-crawl-2015.txt";
         static void Main(string[] args)
         {
+            ConvertFile(ReturnLinkFile());
         }
+
+        static String ReturnLinkFile()
+        {
+            WebClient wc = new WebClient();
+            Stream stream = wc.OpenRead(LINKFILE);
+            StreamReader reader = new StreamReader(stream);
+            return reader.ReadToEnd();
+        }
+
+        static void ConvertFile(string linkfile)
+        {
+            List<String> fileLines = linkfile.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None).ToList();
+
+            Page page = null;
+
+            foreach(string line in fileLines)
+            {
+                
+                if (line.Length > 9 && line.Substring(0, 9) == "Visited: ") 
+                {
+                    if(page != null) visitedPages.Add(page);
+                    page = new Page(line.Trim().Substring(9));  //create new object as page visited
+                }
+                else if (line.Length > 6 && line.Trim().Substring(0, 6) == "Link: ") 
+                {
+                    page.links.Add(line.Trim().Substring(6)); //assume will never be null
+                }
+               
+            }
+
+            visitedPages.Add(page); //last one
+
+        }//ConvertFile
+
+
     }
 }
